bf4f5713aff156913870c9f51a7cb337f167c787
unknown <sarvangill13@gmail.com>
2023-09-11 22:02:14 -0700

    New branch: This branch has new assets so that I can expand what I am doing with the game. We are essentially at the same point as the other branch at the moment, basic movement and a health system but now have more options to expand

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float jumpStrength;
+    public float moveStrength;
+
+    [SerializeField] private Health playerHealth;
+
+    private CapsuleCollider2D capsuleCollider;
+    private Animator anim;
+    private static int distToGround;
+    private int jumps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.name = "Player";
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("grounded", checkGrounded());
+
+        if (Input.GetKeyDown(KeyCode.W) == true || Input.GetKeyDown(KeyCode.Space) == true) 
+        {
+            PlayerJump();
+        }
+
+        if (Input.GetKey(KeyCode.D) == true) 
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+          
+            myRigidBody.velocity = new Vector2(moveStrength, myRigidBody.velocity.y);
+
+            if (anim.GetBool("grounded"))
+            {
+                anim.SetBool("run", true);
+            }
+        }
+        
+        else if (Input.GetKey(KeyCode.A) == true) 
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        
+            myRigidBody.velocity = new Vector2(-1*moveStrength, myRigidBody.velocity.y);
+
+            if (anim.GetBool("grounded"))
+            {
+                anim.SetBool("run", true);
+            }
+        }
+
+        else 
+        {
+            anim.SetBool("run", false);
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (checkGrounded())
+        {
+            jumps = 0;
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpStrength);
+            jumps += 1;
+        }
+        else if (jumps < 2)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpStrength);
+            jumps += 1;
+        }
+
+    }
+
+    bool checkGrounded()
+    {
+        Vector2 position = transform.position;
+        float rayLength = 0.2f;
+        int layerMask = ~(1 << 9); //Exclude layer 9 (Player Layer)
+        RaycastHit2D hit = Physics2D.Raycast(position, Vector2.down, rayLength, layerMask);
+        Debug.DrawLine(position, new Vector2(position.x, position.y - rayLength), Color.magenta, 2.5f);
+
+        if (hit.collider != null && hit.collider.gameObject.name == "Ground")
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Cactus")
+        {
+            playerHealth.takeDamage(1);
+        }
+    }
+}
